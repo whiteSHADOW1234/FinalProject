9a73d730a2975e2bef5e45ca9e6e3cdf752fa739
BangDonKiller <parisdata@gmail.com>
2022-04-21 14:20:03 +0800

    kkkkkk

--- PingPongGame/src/PongGame.java
+++ PingPongGame/src/PongGame.java
@@ -4,7 +4,7 @@ public class PongGame {
 	public static void main(String[] args) {
 //		GameFrame frame = new GameFrame();
 		new GameFrame();
-// new code in here, yoyo added twice.
+// new code in here, yoyo added twice.kkkkk
 // fckkkkk
 	}
 
