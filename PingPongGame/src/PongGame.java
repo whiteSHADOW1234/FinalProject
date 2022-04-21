bdd39af45888f985ee310b5948bd0c494bcc7745
BangDonKiller <parisdata@gmail.com>
2022-04-21 13:09:44 +0800

    yoyo version

--- PingPongGame/src/PongGame.java
+++ PingPongGame/src/PongGame.java
@@ -4,7 +4,7 @@ public class PongGame {
 	public static void main(String[] args) {
 //		GameFrame frame = new GameFrame();
 		new GameFrame();
-// new code in here
+// new code in here, yoyo added.
 // fckkkkk
 	}
 
