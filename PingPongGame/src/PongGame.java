c9f37d906462525dc28a75bb32998fab41d505c0
Ricepaste <n22126@gmail.com>
2022-05-16 19:13:00 +0800

    add the fxml function now

--- PingPongGame/src/PongGame.java
+++ PingPongGame/src/PongGame.java
@@ -1,10 +1,42 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class PongGame {
+public class PongGame extends Application{
 
+	@FXML
+    private Button start;
+
+    @FXML
+    void startgame(ActionEvent event) {
+		GameFrame frame = new GameFrame();
+    }
+	
+	public void start(Stage stage) {
+		try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("final_project_startpage.fxml"));
+
+            Scene background = new Scene(root);
+            stage.setTitle("Greedy Snake");
+            stage.setScene(background);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
 	public static void main(String[] args) {
 		// GameFrame frame = new GameFrame();
-		new GameFrame();
+		// new GameFrame();
 		// start the game function
+
+		launch();
 	}
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FinalProject
-This is the Final Project of 426 dorm
+1. have to add the configuration sentence 
+
+```
+"vmArgs": "--module-path /Users/<user>/Downloads/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.fxml",
+```
+
+in the java configuration
+and it looks like this
+![](https://i.imgur.com/sfXtJg8.png)
