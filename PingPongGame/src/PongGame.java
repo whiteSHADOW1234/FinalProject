963f02df2f0530ecae4e5ca9ff1ba5f9e7bbfd50
Ricepaste <n22126@gmail.com>
2022-05-27 11:46:14 +0800

    music can control the volume now

--- PingPongGame/src/PongGame.java
+++ PingPongGame/src/PongGame.java
@@ -1,10 +1,16 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 // import javafx.application.Platform;
 // import javafx.beans.InvalidationListener;
 // import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,7 +20,7 @@ import javafx.stage.Stage;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 
-public class PongGame extends Application {
+public class PongGame extends Application implements Initializable {
     static int temp;
     SongPlayer DJ = new SongPlayer();
 
@@ -53,11 +59,6 @@ public class PongGame extends Application {
 
     }
 
-
-
-
-
-
     @FXML
     void Setting(ActionEvent event) {
         Stage SETTING = new Stage();
@@ -78,7 +79,8 @@ public class PongGame extends Application {
         System.exit(0);
     }
 
-    public void initialize() {
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
         try {
             if (PongGame.temp == 1) {
                 System.out.println("blue");
@@ -90,6 +92,22 @@ public class PongGame extends Application {
                 situation.setText("Error " + PongGame.temp);
             }
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            System.out.println(volumecontroller.getValue());
+
+            volumecontroller.valueProperty().addListener(new ChangeListener<Number>() {
+
+                @Override
+                public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+
+                    DJ.setVolume(volumecontroller.getValue() * 0.01);
+                }
+            });
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
--- PingPongGame/src/SongPlayer.java
+++ PingPongGame/src/SongPlayer.java
@@ -1,22 +1,58 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+// import java.io.FileInputStream;
+// import java.io.FileNotFoundException;
+
+// import javazoom.jl.decoder.JavaLayerException;
+// import javazoom.jl.player.Player;
 
 public class SongPlayer extends Thread {
+    ArrayList<File> songs;
+    File directory;
+    File[] files;
+    int songNumber;
+    Media media;
+    static MediaPlayer mediaPlayer;
+    
     public void run() {
-        try {
-            FileInputStream fileInputStream = new FileInputStream("PingPongSong.mp3");
-            // here we find our sound file
-            Player player = new Player(fileInputStream);
-            System.out.println("Song is Playing");
-            player.play();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (JavaLayerException e) {
-            e.printStackTrace();
-        }
+        // try {
+        //     FileInputStream fileInputStream = new FileInputStream("PingPongSong.mp3");
+        //     // here we find our sound file
+        //     Player player = new Player(fileInputStream);
+        //     System.out.println("Song is Playing");
+        //     player.play();
+        // } catch (FileNotFoundException e) {
+        //     e.printStackTrace();
+        // } catch (JavaLayerException e) {
+        //     e.printStackTrace();
+        // }
+        songs = new ArrayList<File>();
+		
+		directory = new File("music");
+		
+		files = directory.listFiles();
+		
+		if(files != null) {
+			
+			for(File file : files) {
+				
+				songs.add(file);
+			}
+		}
+		
+		
+        media = new Media(songs.get(songNumber).toURI().toString());
+		mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
     }
+
+    public void setVolume(double volume) {
+        mediaPlayer.setVolume(volume);
+    }
+
 
 }
