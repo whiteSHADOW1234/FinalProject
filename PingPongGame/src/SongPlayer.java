c19ef1a94ee667e73806302c150ff8e6db32f181
Ricepaste <n22126@gmail.com>
2022-05-27 21:31:59 +0800

    delete some comment

--- PingPongGame/src/SongPlayer.java
+++ PingPongGame/src/SongPlayer.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-// import java.io.FileInputStream;
-// import java.io.FileNotFoundException;
-
-// import javazoom.jl.decoder.JavaLayerException;
-// import javazoom.jl.player.Player;
-
 public class SongPlayer extends Thread {
     ArrayList<File> songs;
     File directory;
@@ -17,42 +11,30 @@ public class SongPlayer extends Thread {
     int songNumber;
     Media media;
     static MediaPlayer mediaPlayer;
-    
+
     public void run() {
-        // try {
-        //     FileInputStream fileInputStream = new FileInputStream("PingPongSong.mp3");
-        //     // here we find our sound file
-        //     Player player = new Player(fileInputStream);
-        //     System.out.println("Song is Playing");
-        //     player.play();
-        // } catch (FileNotFoundException e) {
-        //     e.printStackTrace();
-        // } catch (JavaLayerException e) {
-        //     e.printStackTrace();
-        // }
+
         songs = new ArrayList<File>();
-		
-		directory = new File("music");
-		
-		files = directory.listFiles();
-		
-		if(files != null) {
-			
-			for(File file : files) {
-				
-				songs.add(file);
-			}
-		}
-		
-		
+
+        directory = new File("music");
+
+        files = directory.listFiles();
+
+        if (files != null) {
+
+            for (File file : files) {
+
+                songs.add(file);
+            }
+        }
+
         media = new Media(songs.get(songNumber).toURI().toString());
-		mediaPlayer = new MediaPlayer(media);
+        mediaPlayer = new MediaPlayer(media);
         mediaPlayer.play();
     }
 
     public void setVolume(double volume) {
         mediaPlayer.setVolume(volume);
     }
-
 
 }
